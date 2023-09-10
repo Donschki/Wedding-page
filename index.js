0b3b5c03489d78d37a7c6497991e88d6a2453f0d
donschki <donvonkrog@gmail.com>
2023-09-10 16:18:52 +0200

    a

---
+++ index.js
@@ -0,0 +1,80 @@
+// -------------------------------------------------------------------------------------------------------
+
+// CLICK BURGER ICON BURGER-MENU + NAVIGATION-EXPANDED
+
+const burgerIcon = document.querySelector('.burger-icon');
+const burgerMenu = document.querySelector('.burger-menu');
+const line1 = document.querySelector('.line-1');
+const line2 = document.querySelector('.line-2');
+const line3 = document.querySelector('.line-3');
+
+burgerIcon.addEventListener ('click', function() {
+
+     burgerMenu.classList.toggle ('menu-open');
+     line1.classList.toggle('line1');
+     line2.classList.toggle('line2');
+     line3.classList.toggle('line3');
+
+});
+
+// -------------------------------------------------------------------------------------------------------
+
+// SLIDESHOW
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+// -------------------------------------------------------------------------------------------------------
+
+// COUNTDOWN TIMER (days.hours.minutes.seconds)
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d" + " " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+// -------------------------------------------------------------------------------------------------------
+
+
